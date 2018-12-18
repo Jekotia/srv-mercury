704bb797bd1bcc06faaba04a505b46078c56fb4a
Josh 'Jekotia' Ameli <contact@jekotia.net>
2018-12-18 11:15:11 -0500

    Remove accidential attempt to execute log file

--- stackscript.sh
+++ stackscript.sh
@@ -4,5 +4,4 @@ mkdir -p /srv/logs
 apt install -y git > /srv/logs/setup.log 2>&1
 
 git clone https://github.com/Jekotia/srv-mercury.git /srv/mercury >> /srv/logs/setup.log 2>&1
-/srv/logs/setup.log
 bash /srv/mercury/setup.sh >> /srv/logs/setup.log 2>&1
